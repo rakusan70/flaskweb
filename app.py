588f94fbddcfb4932ca5791caa52822fa37beccd
rakusan70 <damik.f@gmail.com>
2019-09-05 12:08:34 +0000

    edit app.py and run.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ flask_app = Flask(__name__)
 @flask_app.route("/")
 def index():
     return "Hello World"
+
+@flask_app.route("/admin/")
+def admin():
+    return "Hello Admin"
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import flask_app
 
 if __name__ =="__main__":
-    debug = False
+    debug = True
     host = "0.0.0.0"
     flask_app.run(host, debug=debug)
